e1c8874d0be39b34e620d4c46c6c778ee2034637
Dawon-yoon <dawon3887@gmail.com>
2024-07-21 21:57:27 +0900

    add open sidebar button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons';
 
 const Nav = () => {
+  const [isSidebarOpen, SetIsSidebarOpen] = useState(false);
+
+  const handleSidebarToggle = () => { 
+    SetIsSidebarOpen(!isSidebarOpen);
+  }
+
   return (
-      <div className='navbar'>
-          <div className='menus'>
+    <div>
+    <div className='navbar'> 
+      <div
+        className='show'
+        onClick={handleSidebarToggle}
+      >
+        {isSidebarOpen? (<FontAwesomeIcon icon={faX} />):(<div><FontAwesomeIcon icon={faBars} /></div>)}
+      </div> 
+          <div className='menus hidden'>
               <div>about</div>
               <div>skill</div>
               <div>project</div>
               <div>contact</div>
           </div>   
+      </div>
+      <div className={`sidebar ${isSidebarOpen ? 'open' : 'close'}`}>
+        <div>
+          <span>about</span>
+          <span>skill</span>
+          <span>project</span>
+          <span>contact</span>
+        </div>
+      </div>
     </div>
   )
 }
