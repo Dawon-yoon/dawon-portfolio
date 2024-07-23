1efdd45068b03a851ad21e1fc87ba4dcd7eb772f
Dawon-yoon <dawon3887@gmail.com>
2024-07-23 10:36:59 +0900

    modified deploy error

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,6 +1,4 @@
 import React,{useEffect} from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faAnglesDown} from '@fortawesome/free-solid-svg-icons'
 import '../scss/intro.scss';
 import Stars from './Stars';
 
